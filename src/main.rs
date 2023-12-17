c22d38aafcee72d3f002125d52156bb5b77fe31d
Guilherme Gonçalves <guilherme16.gon@gmail.com>
2023-12-17 16:25:43 -0300

    Add CORS support

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,9 @@ use routes::transaction_routes;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let cors = Cors::permissive();
         App::new()
+            .wrap(cors)
             .configure(transaction_routes::config)
     })
     .bind("127.0.0.1:8080")?
